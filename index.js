8f85729230277d3d0d4ba54076551f1a3fc865c1
yong liang <yongliang24@gmail.com>
2018-11-03 04:30:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animal;
 
 function myAnimal() {
-  animal == 'dog';
+  animal = 'dog';
     return animal;
 
 
@@ -11,7 +11,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  animal== 'cat';
+  animal= 'cat';
     return animal;
   }
 
